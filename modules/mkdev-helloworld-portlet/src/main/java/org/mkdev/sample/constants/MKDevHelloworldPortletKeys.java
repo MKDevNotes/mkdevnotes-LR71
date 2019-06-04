8355a824b29e5ff341790368eeaa32b72a3cf352
Muthukumar Thangavinayagam <2063172+mkpracticalworks@users.noreply.github.com>
2019-06-04 22:53:07 +0530

    Hello World Sample Code commit

---
+++ modules/mkdev-helloworld-portlet/src/main/java/org/mkdev/sample/constants/MKDevHelloworldPortletKeys.java
@@ -0,0 +1,10 @@
+package org.mkdev.sample.constants;
+
+/**
+ * @author Muthukumar
+ */
+public class MKDevHelloworldPortletKeys {
+
+	public static final String MKDevHelloworld = "mkdevhelloworld";
+
+}
---
+++ modules/mkdev-helloworld-portlet/src/main/java/org/mkdev/sample/portlet/MKDevHelloworldPortlet.java
@@ -0,0 +1,28 @@
+package org.mkdev.sample.portlet;
+
+import org.mkdev.sample.constants.MKDevHelloworldPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Muthukumar
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + MKDevHelloworldPortletKeys.MKDevHelloworld,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class MKDevHelloworldPortlet extends MVCPortlet {
+}
